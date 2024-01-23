5cfc13b472a71ca127f7d474538371221cc56a1e
4eJIoBeG <pavelshashenko@gmail.com>
2024-01-24 00:11:06 +0300

    add color picker and fix color bug in state

--- client/src/components/ColorPicker.jsx
+++ client/src/components/ColorPicker.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { SketchPicker } from "react-color";
+import { useSnapshot } from "valtio";
+
+import state from "../store";
 
 const ColorPicker = () => {
-  return <div>ColorPicker</div>;
+  const snap = useSnapshot(state);
+
+  return (
+    <div className="absolute left-full ml-3">
+      <SketchPicker
+        color={snap.shirtColor}
+        disableAlpha
+        presetColors={[]}
+        onChange={(color) => (state.shirtColor = color.hex)}
+      />
+    </div>
+  );
 };
 
 export default ColorPicker;
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -3,6 +3,7 @@ import { proxy } from "valtio";
 const state = proxy({
   intro: true,
   color: "#EFBD48",
+  shirtColor: "#EFBD48",
   isLogoTexture: true,
   isFullTexture: false,
   logoDecal: "./threejs.png",
